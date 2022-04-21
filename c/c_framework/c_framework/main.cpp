c9144beb00227dffb9c41a93229543ab605f654c
jodrun <a9011291094810@gmail.com>
2022-04-21 22:55:39 +0900

    c_framework0.3

--- c/c_framework/c_framework/main.cpp
+++ c/c_framework/c_framework/main.cpp
@@ -1,16 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string>
 #include <malloc.h>
-#include <stdlib.h>
+#include <string>
 #include <time.h>
-#include <windows.h> //sleep 함수 사용할때 필요
-//Sleep(500); 0.5초만큼 딜레이줌
+#include <Windows.h>
 
 
 const int PLAYER = 0;
 const int ENEMY = 1;
 const int MAX = 2;
+
+const int Scene_Logo = 0;
+const int Scene_Menu = 1;
+const int Scene_Stage = 2;
+const int Scene_Exit = 3;
+
+int SceneState = 0;
+
+
+
+
+
 
 
 typedef struct tagInfo
@@ -36,80 +46,62 @@ typedef struct tagObject
 }OBJECT;
 
 
+OBJECT* Objects[MAX];
 
 
+
+
+
+
+
+void SceneManager(int _SceneState);
 void InitializeObject(OBJECT* _Obj, int ObjectType);
 char* SetName();
+
+void LogoScene();
+void MenuScene();
+void StageScene();
+
 
 
 int main(void)
 {
-	/*
-	OBJECT* Player = (OBJECT*)malloc(sizeof(OBJECT));
-	InitializeObject(Player, PLAYER);
-
-	OBJECT* Enemy = (OBJECT*)malloc(sizeof(OBJECT));;
-	InitializeObject(Enemy, ENEMY);
-	*/
-
-	OBJECT* Objects[MAX];
-
 	Objects[PLAYER] = (OBJECT*)malloc(sizeof(OBJECT));
 	InitializeObject(Objects[PLAYER], PLAYER);
 
 	Objects[ENEMY] = (OBJECT*)malloc(sizeof(OBJECT));
 	InitializeObject(Objects[ENEMY], ENEMY);
 
+	SceneState = 0;
 
 	while (true)
 	{
-		// ** 콘솔창을 모두 지움.
-		system("cls");
-
-		printf_s("\n[%s]\n", Objects[PLAYER]->Info.Name);
-		printf_s("HP : %d\n", Objects[PLAYER]->Info.HP);
-		printf_s("MP : %d\n", Objects[PLAYER]->Info.MP);
-		printf_s("공격력 : %.2f\n", Objects[PLAYER]->Info.Att);
-		printf_s("방어력 : %.2f\n", Objects[PLAYER]->Info.Def);
-		printf_s("EXP : %d\n", Objects[PLAYER]->Info.EXP);
-		printf_s("Level : %d\n\n", Objects[PLAYER]->Info.Level);
-
-		Sleep(500);
-
-		printf_s("[%s]\n", Objects[ENEMY]->Info.Name);
-		printf_s("HP : %d\n", Objects[ENEMY]->Info.HP);
-		printf_s("MP : %d\n", Objects[ENEMY]->Info.MP);
-		printf_s("공격력 : %.2f\n", Objects[ENEMY]->Info.Att);
-		printf_s("방어력 : %.2f\n", Objects[ENEMY]->Info.Def);
-		printf_s("EXP : %d\n", Objects[ENEMY]->Info.EXP);
-		printf_s("Level : %d\n\n", Objects[ENEMY]->Info.Level);
-
-		Sleep(500);
-
-		int iChoice = 0;
-		printf_s("몬스터와 만났습니다. 공격하시겠습니까 ?\n1. 공격\n2. 도망\n입력 : ");
-		scanf_s("%d", &iChoice);
-
-		switch (iChoice)
-		{
-		case 1:
-			if (Objects[PLAYER]->Info.Att > Objects[ENEMY]->Info.Def)
-			{
-				Objects[ENEMY]->Info.HP -= Objects[PLAYER]->Info.Att - Objects[ENEMY]->Info.Def;
-			}
-			else
-				Objects[ENEMY]->Info.HP -= 1;
-			break;
-		case 2:
-			printf_s("도망치는것에 [성공] 했습니다.\n");
-			break;
-		}
+		SceneManager(SceneState);
 	}
 
 	return 0;
 }
 
 
+void SceneManager(int _SceneState)
+{
+	switch (_SceneState)
+	{
+	case Scene_Logo:
+		LogoScene();
+		break;
+	case Scene_Menu:
+		MenuScene();
+		break;
+	case Scene_Stage:
+		StageScene();
+		break;
+	case Scene_Exit:
+
+		exit(NULL);// ** 프로그램 종료
+		break;
+	}
+}
 
 
 void InitializeObject(OBJECT* _Obj, int ObjectType)
@@ -139,15 +131,110 @@ void InitializeObject(OBJECT* _Obj, int ObjectType)
 	}
 }
 
-char* SetName()
+char* SetName()    //포인터로 반환값을 입력했기에 리턴을 포인터로 받아야하니 동적할당 해줘야하고 strcpy 사용해줘야함
 {
 	char Buffer[128] = "";
 
 	printf_s("이름 입력 : ");
 	scanf("%s", Buffer);
 
-	char* pName = (char*)malloc(strlen(Buffer) + 1);
-	strcpy(pName, Buffer);
+	char* pName = (char*)malloc(strlen(Buffer) + 1); //포인터 pname을 만들어주고 buffer가 배열이니 타입변환 해주면서 동적할당
+	strcpy(pName, Buffer); //배열 buffer에 입력된 이름값을 포인터 pname에 넘겨줌
 
-	return pName;
+	return pName; //즉 이름값이 배열에 들어갔으니 포인터로 변환해주는 과정을 거쳤음 그과정에서 (동적할당, strcpy) 사용됨
+}
+
+void LogoScene()
+{
+	printf_s("LogoScene\n");
+	SceneState++;
+}
+
+void MenuScene()
+{
+	printf_s("MenuScene\n");
+
+	printf_s("다음 씬 ㄱㄱ??\n1. 이동\n2. 종료\n일력 : ");
+
+	int i = 0;
+	scanf("%d", &i);
+
+
+	if (i == 1)
+		SceneState++;
+	else if (i == 2)
+		SceneState = Scene_Exit;
+}
+
+void StageScene()
+{
+	int iLoopCheck = 1;
+	while (iLoopCheck)
+	{
+		// ** 콘솔창을 모두 지움.
+		//system("cls");
+
+		printf_s("\n[%s]\n", Objects[PLAYER]->Info.Name);
+		printf_s("HP : %d\n", Objects[PLAYER]->Info.HP);
+		printf_s("MP : %d\n", Objects[PLAYER]->Info.MP);
+		printf_s("공격력 : %.2f\n", Objects[PLAYER]->Info.Att);
+		printf_s("방어력 : %.2f\n", Objects[PLAYER]->Info.Def);
+		printf_s("EXP : %d\n", Objects[PLAYER]->Info.EXP);
+		printf_s("Level : %d\n\n", Objects[PLAYER]->Info.Level);
+
+		printf_s("[%s]\n", Objects[ENEMY]->Info.Name);
+		printf_s("HP : %d\n", Objects[ENEMY]->Info.HP);
+		printf_s("MP : %d\n", Objects[ENEMY]->Info.MP);
+		printf_s("공격력 : %.2f\n", Objects[ENEMY]->Info.Att);
+		printf_s("방어력 : %.2f\n", Objects[ENEMY]->Info.Def);
+		printf_s("EXP : %d\n", Objects[ENEMY]->Info.EXP);
+		printf_s("Level : %d\n\n", Objects[ENEMY]->Info.Level);
+
+		// ** 딜레이 함수   1/1000   (1000 = 1초)
+		Sleep(500);
+
+		int iChoice = 0;
+		printf_s("몬스터와 만났습니다. 공격하시겠습니까 ?\n1. 공격\n2. 도망\n입력 : ");
+		scanf_s("%d", &iChoice);
+
+
+		switch (iChoice)
+		{
+		case 1:
+			if (Objects[PLAYER]->Info.Att > Objects[ENEMY]->Info.Def)
+			{
+				Objects[ENEMY]->Info.HP -= Objects[PLAYER]->Info.Att - Objects[ENEMY]->Info.Def;
+			}
+			else
+				Objects[ENEMY]->Info.HP -= 1;
+
+			if (Objects[ENEMY]->Info.Att > Objects[PLAYER]->Info.Def)
+			{
+				Objects[PLAYER]->Info.HP -= Objects[ENEMY]->Info.Att - Objects[PLAYER]->Info.Def;
+			}
+			else
+				Objects[PLAYER]->Info.HP -= 1;
+
+			iLoopCheck = 0;
+			break;
+		case 2:
+
+			int iRand = rand() % 100;
+
+			if (iRand < 10)
+			{
+				printf_s("도망치는것에 [성공] 했습니다.\n");
+				iLoopCheck = 0;
+			}
+			else
+			{
+				printf_s("도망치는것에 [실패] 했습니다.\n");
+				printf_s("전투가 시작됩니다.\n");
+				iChoice = 1;
+			}
+
+			Sleep(1500);
+			break;
+		}
+	}
 }
