a42480473efb555d3cc99c14c16e2ff18360142f
jodrun <a9011291094810@gmail.com>
2022-05-09 15:26:37 +0900

    c_textrpg0.4

--- c/c_textrpg/c_textrpg/main.cpp
+++ c/c_textrpg/c_textrpg/main.cpp
@@ -13,10 +13,13 @@ const int Scene_Store     = 2;
 const int Scene_Inventory = 3;  
 const int Scene_Exit      = 4;
 
+int GetGold = 0;
 
 int SceneState = 0;
 int iStageScene = 0;
 int act1choice = 0;
+
+
 
 typedef struct _Iventory
 {
@@ -26,7 +29,9 @@ typedef struct _Iventory
 typedef struct tagInfo
 {
 	int HP;
+	int HPMAX;
 	int MP;
+	int MPMAX;
 
 	int EXP;
 
@@ -49,26 +54,39 @@ typedef struct tagObject
 
 
 
-void SceneManager(OBJECT* _Player, OBJECT* _Enemy);
+void SceneManager(OBJECT* _Player, OBJECT* _Enemy, OBJECT* _Enemy1, OBJECT* _Enemy2);
 char* SetName();
 
 void LogoScene();
 void MenuScene();
-void StageScene(OBJECT* _Player, OBJECT* _Enemy);
+void StageScene(OBJECT* _Player, OBJECT* _Enemy, OBJECT* _Enemy1, OBJECT* _Enemy2);
 void act1(OBJECT* _Player, OBJECT* _Enemy);
+void act2(OBJECT* _Player, OBJECT* _Enemy1);
+void act3(OBJECT* _Player, OBJECT* _Enemy2);
 void StoreScene();
 void InventoryScene();
 
 void InitializePlayer(OBJECT* _Player);
-void PlayerScene(OBJECT* _Player, OBJECT* _Enemy);
+void PlayerSceneact1(OBJECT* _Player, OBJECT* _Enemy);
+void PlayerSceneact2(OBJECT* _Player, OBJECT* _Enemy1);
+void PlayerSceneact3(OBJECT* _Player, OBJECT* _Enemy2);
 void InitializeEnemy(OBJECT* _Enemy);
-void EnemyScene(OBJECT* _Enemy, OBJECT* _Player);
+void InitializeEnemy1(OBJECT* _Enemy1);
+void InitializeEnemy2(OBJECT* _Enemy2);
+void EnemySceneact1(OBJECT* _Enemy, OBJECT* _Player);
+void EnemySceneact2(OBJECT* _Enemy1, OBJECT* _Player);
+void EnemySceneact3(OBJECT* _Enemy2, OBJECT* _Player);
 
 void pHealSkill(OBJECT* _Player);
 void mHealSkill(OBJECT* _Enemy);
 
 void SetPosition(int _x, int _y, char* _str, int _Color = 15);
 void SetColor(int _Color);
+
+
+
+
+
 
 
 int main(void)
@@ -85,6 +103,13 @@ int main(void)
 	OBJECT* Monster = (OBJECT*)malloc(sizeof(OBJECT));
 	InitializeEnemy(Monster);
 	
+	OBJECT* Monster1 = (OBJECT*)malloc(sizeof(OBJECT));
+	InitializeEnemy1(Monster1);
+
+	OBJECT* Monster2 = (OBJECT*)malloc(sizeof(OBJECT));
+	InitializeEnemy2(Monster2);
+
+
 	DWORD dwTime = GetTickCount();
 	int Delay = 1000;
 
@@ -99,7 +124,7 @@ int main(void)
 			printf_s("%s님\n\n", Player->Name);
 
 			// ** 게임 루프
-			SceneManager(Player, Monster);
+			SceneManager(Player, Monster, Monster1, Monster2);
 		}
 	}
 
@@ -109,7 +134,11 @@ int main(void)
 
 
 
-void SceneManager(OBJECT* _Player, OBJECT* _Enemy)
+
+
+
+
+void SceneManager(OBJECT* _Player, OBJECT* _Enemy, OBJECT* _Enemy1, OBJECT* _Enemy2)
 {
 	switch (SceneState)
 	{
@@ -117,7 +146,7 @@ void SceneManager(OBJECT* _Player, OBJECT* _Enemy)
 		MenuScene();
 		break;
 	case Scene_Stage:
-		StageScene(_Player, _Enemy);
+		StageScene(_Player, _Enemy, _Enemy1, _Enemy2);
 		break;
 	case Scene_Store:
 		StoreScene();
@@ -181,7 +210,7 @@ void MenuScene()
 		SceneState = Scene_Exit;
 }
 
-void StageScene(OBJECT* _Player, OBJECT* _Enemy)
+void StageScene(OBJECT* _Player, OBJECT* _Enemy, OBJECT* _Enemy1, OBJECT* _Enemy2)
 {
 	printf_s("스테이지를 고르세요\n\n");
 	printf_s("1, 스테이지1(쉬움)\n");
@@ -196,6 +225,14 @@ void StageScene(OBJECT* _Player, OBJECT* _Enemy)
 	{
 		act1(_Player, _Enemy);
 	}
+	else if (iStageScene == 2)
+	{
+		act2(_Player, _Enemy1);
+	}
+	else if (iStageScene == 3)
+	{
+		act3(_Player, _Enemy2);
+	}
 	else if (iStageScene == 4)
 	{
 		SceneState--;
@@ -208,18 +245,76 @@ void act1(OBJECT* _Player, OBJECT* _Enemy)
 	printf_s("1, 스테이지1(쉬움)\n\n");
 	Sleep(2000);
 	system("cls");
+	act1choice = 0;
 
 	while (true)
 	{
 		if (act1choice == 0)
 		{
-			PlayerScene(_Player, _Enemy);
+			PlayerSceneact1(_Player, _Enemy);
 		}
 		else if (act1choice == 1)
 		{
-			EnemyScene(_Enemy, _Player);
+			EnemySceneact1(_Enemy, _Player);
 		}
+		else if (act1choice == 9) //도망성공
+		{
+			break;
+		}
+		GetGold = 0;
 	}
+}
+
+void act2(OBJECT* _Player, OBJECT* _Enemy1)
+{
+	printf_s("2, 스테이지2(보통)\n\n");
+	Sleep(2000);
+	system("cls");
+	act1choice = 0;
+
+	
+	while (true)
+	{
+		if (act1choice == 0)
+		{
+			PlayerSceneact2(_Player, _Enemy1);
+		}
+		else if (act1choice == 1)
+		{
+			EnemySceneact2(_Enemy1, _Player);
+		}
+		else if (act1choice == 9) //도망성공
+		{
+			break;
+		}
+		GetGold = 0;
+	}
+}
+
+void act3(OBJECT* _Player, OBJECT* _Enemy2)
+{
+	printf_s("3, 스테이지3(어려움)\n\n");
+	Sleep(2000);
+	system("cls");
+	act1choice = 0;
+
+	while (true)
+	{
+		if (act1choice == 0)
+		{
+			PlayerSceneact3(_Player, _Enemy2);
+		}
+		else if (act1choice == 1)
+		{
+			EnemySceneact3(_Enemy2, _Player);
+		}
+		else if (act1choice == 9) //도망성공
+		{
+			break;
+		}
+		GetGold = 0;
+	}
+
 }
 
 void StoreScene()
@@ -236,16 +331,18 @@ void InitializePlayer(OBJECT* _Player)
 {
 	_Player->Name = SetName();
 
-	_Player->Info.Att = 10;
+	_Player->Info.Att = 30;
 	_Player->Info.Def = 10;
 	_Player->Info.EXP = 0;
 	_Player->Info.HP = 100;
+	_Player->Info.HPMAX = 100;
 	_Player->Info.MP = 10;
+	_Player->Info.MPMAX = 10;
 	_Player->Info.Level = 1;
 	_Player->iIventory.Gold = 0;
 }
 
-void PlayerScene(OBJECT* _Player, OBJECT* _Enemy)
+void PlayerSceneact1(OBJECT* _Player, OBJECT* _Enemy)
 {
 	printf_s("%s님\n", _Player->Name);
 	printf_s("Player Level : %d\n", _Player->Info.Level);
@@ -255,15 +352,17 @@ void PlayerScene(OBJECT* _Player, OBJECT* _Enemy)
 	printf_s("Player Def : %d\n", _Player->Info.Def);
 	printf_s("Player EXP : %d\n\n", _Player->Info.EXP);
 	printf_s("보유 Gold : %d\n\n", _Player->iIventory.Gold);
-	printf_s("1, 공격\n2, 방어\n3, 힐\n입력 : ");
+	printf_s("1, 공격\n2, 방어\n3, 힐\n9, 도망가기\n입력 : ");
 	
 
 	int sPlayerChoice = 0;
 	scanf("%d", &sPlayerChoice);
 	printf_s("\n");
-	
+
 	if (sPlayerChoice == 1) //플레이어 공격
 	{
+		//삼항연산자로도 할수있음 최소값 줄수있음
+		//_Player->Info.Att = (_Player->Info.Att <= 0 ? 1 : _Player->Info.Att;)
 		if (_Player->Info.Att - (_Enemy->Info.Def / 5) <= 0)
 		{
 			--_Enemy->Info.HP;
@@ -299,36 +398,142 @@ void PlayerScene(OBJECT* _Player, OBJECT* _Enemy)
 	}
 	else if (sPlayerChoice == 3) //플레이어 힐
 	{
-		if (_Player->Info.MP >= 5)
+		if (_Player->Info.MP >= 5 && _Player->Info.HP < _Player->Info.HPMAX)
 		{
 			pHealSkill(_Player);
 			act1choice++;
 		}
+		else if (_Player->Info.HP >= _Player->Info.HPMAX)
+		{
+			printf_s("HP가 가득 찼습니다\n");
+			Sleep(2000);
+		}
 		else
 		{
-			printf_s("MP가 부족합니다");
+			printf_s("MP가 부족합니다\n");
 			Sleep(2000);
 		}
 		system("cls");
 	}
-	
+    else if (sPlayerChoice == 9) //플레이어 도망
+	{
+		srand(time(NULL));
+		int runaway = rand() % 3 + 1;
+		if (runaway == 1)
+		{
+			printf_s("도망성공\n");
+			Sleep(2000);
+			_Player->Info.HP = _Player->Info.HPMAX;
+			_Player->Info.MP = _Player->Info.MPMAX;
+			act1choice = 9;
+		}
+		else
+		{
+			printf_s("도망실패\n");
+			Sleep(2000);
+			act1choice++;
+		}
+	}
+
+	//자동 마나회복
+	if (sPlayerChoice == 3)
+	{
+
+	}
+	else
+	{
+		if (_Player->Info.MP + 2 <= _Player->Info.MPMAX)
+			_Player->Info.MP += 2;
+		else if (_Player->Info.MP + 1 <= _Player->Info.MPMAX)
+			_Player->Info.MP += 1;
+	}
+		
+
+	//몬스터 처치시
+	if (_Enemy->Info.HP <= 0)
+	{
+		srand(time(NULL));
+		GetGold = rand() % (_Enemy->Info.GoldMax - _Enemy->Info.GoldMin + 1) + _Enemy->Info.GoldMin;
+		_Player->iIventory.Gold += GetGold;
+		_Player->Info.EXP += _Enemy->Info.EXP;
+
+		printf_s("\n적을 처치하였습니다.\n");
+		printf_s("획득 골드 : %d\n", GetGold);
+		printf_s("획득 경험치 : %d\n", _Enemy->Info.EXP);
+		Sleep(3000);
+		
+		_Enemy->Info.HP = _Enemy->Info.HPMAX;
+		_Enemy->Info.MP = _Enemy->Info.MPMAX;
+		_Player->Info.HP = _Player->Info.HPMAX;
+		_Player->Info.MP = _Player->Info.MPMAX;
+
+		act1choice = 9;
+	}
+
+
+
+}
+
+void PlayerSceneact2(OBJECT* _Player, OBJECT* _Enemy1)
+{
+
+}
+
+void PlayerSceneact3(OBJECT* _Player, OBJECT* _Enemy2)
+{
+
 }
 
 void InitializeEnemy(OBJECT* _Enemy)
 {
-	_Enemy->Name = (char*)"오크";
+	_Enemy->Name = (char*)"약한적";
 
-	_Enemy->Info.Att = 5;
-	_Enemy->Info.Def = 30;
-	_Enemy->Info.EXP = 0;
+	_Enemy->Info.Att = 10;
+	_Enemy->Info.Def = 40;
+	_Enemy->Info.EXP = 50;
 	_Enemy->Info.HP = 30;
+	_Enemy->Info.HPMAX = 30;
 	_Enemy->Info.MP = 5;
+	_Enemy->Info.MPMAX = 5;
 	_Enemy->Info.Level = 1;
 	_Enemy->Info.GoldMin = 5;
 	_Enemy->Info.GoldMax = 30;
 }
 
-void EnemyScene(OBJECT* _Enemy, OBJECT* _Player)
+void InitializeEnemy1(OBJECT* _Enemy1)
+{
+	_Enemy1->Name = (char*)"보통적";
+		  
+	_Enemy1->Info.Att = 20;
+	_Enemy1->Info.Def = 40;
+	_Enemy1->Info.EXP = 50;
+	_Enemy1->Info.HP = 30;
+	_Enemy1->Info.HPMAX = 30;
+	_Enemy1->Info.MP = 5;
+	_Enemy1->Info.MPMAX = 5;
+	_Enemy1->Info.Level = 1;
+	_Enemy1->Info.GoldMin = 5;
+	_Enemy1->Info.GoldMax = 30;
+}
+
+void InitializeEnemy2(OBJECT* _Enemy2)
+{
+	_Enemy2->Name = (char*)"강한적";
+		  
+	_Enemy2->Info.Att = 20;
+	_Enemy2->Info.Def = 40;
+	_Enemy2->Info.EXP = 50;
+	_Enemy2->Info.HP = 30;
+	_Enemy2->Info.HPMAX = 30;
+	_Enemy2->Info.MP = 5;
+	_Enemy2->Info.MPMAX = 5;
+	_Enemy2->Info.Level = 1;
+	_Enemy2->Info.GoldMin = 5;
+	_Enemy2->Info.GoldMax = 30;
+}		  
+
+
+void EnemySceneact1(OBJECT* _Enemy, OBJECT* _Player)
 {
 
 	printf_s("적 차례 입니다.\n");
@@ -363,6 +568,27 @@ void EnemyScene(OBJECT* _Enemy, OBJECT* _Player)
 				printf_s("데미지 : %d\n", _Enemy->Info.Att - _Player->Info.Def / 5);
 				printf_s("Player HP : %d\n", _Player->Info.HP);
 			}
+
+			if (_Player->Info.HP <= 0) //플레이어 죽음
+			{
+				_Enemy->Info.HP = _Enemy->Info.HPMAX;
+				_Enemy->Info.MP = _Enemy->Info.MPMAX;
+				_Player->Info.HP = _Player->Info.HPMAX;
+				_Player->Info.MP = _Player->Info.MPMAX;
+
+				_Player->Info.EXP -= (_Player->Info.EXP * 0.1f);
+				
+				Sleep(2000);
+				system("cls");
+				printf_s("\n적에게 당했습니다.\n");
+				printf_s("\n경험치를 잃었습니다.\n");
+				Sleep(3000);
+				
+
+				act1choice = 9;
+				break;
+			}
+
 			Sleep(2000);
 			act1choice = 0;
 			system("cls");
@@ -370,12 +596,16 @@ void EnemyScene(OBJECT* _Enemy, OBJECT* _Player)
 		}
 		else if (sEnemyChoice == 2) //에너미 힐
 		{
-			if (_Enemy->Info.MP >= 5)
+			if (_Enemy->Info.MP >= 5 && _Enemy->Info.HP < _Enemy->Info.HPMAX)
 			{
 				mHealSkill(_Enemy);
 				act1choice = 0;
 				system("cls");
 				break;
+			}
+			else if (_Enemy->Info.HP >= _Enemy->Info.HPMAX)
+			{
+				sEnemyChoice = 1;
 			}
 			else
 			{
@@ -387,11 +617,30 @@ void EnemyScene(OBJECT* _Enemy, OBJECT* _Player)
 	}
 }
 
+void EnemySceneact2(OBJECT* _Enemy1, OBJECT* _Player)
+{
+
+}
+
+void EnemySceneact3(OBJECT* _Enemy2, OBJECT* _Player)
+{
+
+}
+
 void pHealSkill(OBJECT* _Player)
 {
 	_Player->Info.MP -= 5;
-	_Player->Info.HP += 10;
-	printf_s("힐량 : %d\n", 10);
+	if (_Player->Info.HP + 10 <= _Player->Info.HPMAX) 
+	{
+		printf_s("힐량 : %d\n", 10);
+		_Player->Info.HP += 10;
+	}
+	else if (_Player->Info.HP + 10 > _Player->Info.HPMAX && _Player->Info.HP < _Player->Info.HPMAX)
+	{
+		printf_s("힐량 : %d\n", _Player->Info.HPMAX - _Player->Info.HP);
+		_Player->Info.HP += _Player->Info.HPMAX - _Player->Info.HP;
+	}
+	
 	printf_s("Player HP : %d\n", _Player->Info.HP);
 	Sleep(2000);
 }
@@ -399,8 +648,17 @@ void pHealSkill(OBJECT* _Player)
 void mHealSkill(OBJECT* _Enemy)
 {
 	_Enemy->Info.MP -= 5;
-	_Enemy->Info.HP += 10;
-	printf_s("힐량 : %d\n", 10);
+	if (_Enemy->Info.HP + 10 <= _Enemy->Info.HPMAX)
+	{
+		printf_s("힐량 : %d\n", 10);
+		_Enemy->Info.HP += 10;
+	}
+	else if (_Enemy->Info.HP + 10 > _Enemy->Info.HPMAX && _Enemy->Info.HP < _Enemy->Info.HPMAX)
+	{
+		printf_s("힐량 : %d\n", _Enemy->Info.HPMAX - _Enemy->Info.HP);
+		_Enemy->Info.HP += _Enemy->Info.HPMAX - _Enemy->Info.HP;
+	}
+	
 	printf_s("Enemy HP : %d\n", _Enemy->Info.HP);
 	Sleep(2000);
 }
