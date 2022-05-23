c9b93781210d7853fbd3ffc0339519d832728fdd
jodrun <a9011291094810@gmail.com>
2022-05-23 15:25:26 +0900

    c_frameworkstudy0.3

--- c/c_frameworkstudy0.3/c_frameworkstudy0.3/DEFIND.h
+++ c/c_frameworkstudy0.3/c_frameworkstudy0.3/DEFIND.h
@@ -1,31 +1,34 @@
 #pragma once
 
 // ** 초기화 함수 (디폴트 매개변수 : int _Value = 0)
-void Initialize(Object* _Object, char* _Texture, int _PosX = 0, int _PosY = 0, int _PosZ = 0);
+void Initialize(Object* _Object, char* _Texture, float _PosX = 0, float _PosY = 0, float _PosZ = 0);
 
 // ** 이름을 셋팅하는 함수
 char* SetName();
 
 // ** 커서의 위치를 변경
-void SetCursorPosition(const int _x, const int _y);
+void SetCursorPosition(const float _x, const float _y);
 
 // ** Text의 색을 변경함.
 void SetTextColor(const int _Color);
 
 // ** 출력할 Text의 위치와 색상을 변경해준다. [Color 값은 기본값 : 흰색(15)]
-void OnDrawText(const char* _str, const int _x, const int _y, const int _Color = 15);
+void OnDrawText(const char* _str, const float _x, const float _y, const int _Color = 15);
 
 // ** 출력할 숫자의 위치와 색상을 변경해준다. [Color 값은 기본값 : 흰색(15)]
-void OnDrawText(const int _Value, const int _x, const int _y, const int _Color = 15);
+void OnDrawText(const int _Value, const float _x, const float _y, const int _Color = 15);
 
 // ** 커서를 보이거나(true) / 안보이게(false) 만들어줌.
 void HideCursor(const bool _Visible);
 
 // ** 충돌처리를 함.
-void Collision(const Object* _ObjectA, const Object* _ObjectB);
+bool Collision(const Object* _ObjectA, const Object* _ObjectB);
 
 // ** Bullet를 생성함.
-Object* CreateBullet(const int _x, const int _y);
+Object* CreateBullet(const float _x, const float _y);
+
+// ** Enemy를 생성함.
+Object* CreateEnemy(const float _x, const float _y);
 
 // ** 키입력 
 void UpdateInput(Object* _Object);
@@ -35,13 +38,10 @@ void UpdateInput(Object* _Object);
 
 
 
-
-
-
 // ** 함수 정의부
 
 
-void Initialize(Object* _Object, char* _Texture, int _PosX, int _PosY, int _PosZ)
+void Initialize(Object* _Object, char* _Texture, float _PosX, float _PosY, float _PosZ)
 {
 	// ** 3항 연상자. 
 	// ** _Name 의 값이 nullptr 이라면  SetName() 함수를 실행하고 아니라면,
@@ -55,11 +55,11 @@ void Initialize(Object* _Object, char* _Texture, int _PosX, int _PosY, int _PosZ
 	_Object->TransInfo.Position = Vector3(_PosX, _PosY, _PosZ);
 
 	// ** 회전값 (현재 사용되지 않음.)
-	_Object->TransInfo.Rotation = Vector3(0, 0, 0);
+	_Object->TransInfo.Rotation = Vector3(0.0f, 0.0f, 0.0f);
 
 	// ** 크기값
 	_Object->TransInfo.Scale = Vector3(
-		strlen(_Object->Info.Texture), 1, 0);
+		(float)strlen(_Object->Info.Texture), 1.0f, 0.0f);
 }
 
 char* SetName()
@@ -81,7 +81,7 @@ char* SetName()
 	return pName;
 }
 
-void SetCursorPosition(const int _x, const int _y)
+void SetCursorPosition(const float _x, const float _y)
 {
 	COORD Pos = { (SHORT)_x, (SHORT)_y };
 
@@ -95,14 +95,14 @@ void SetTextColor(const int _Color)
 		GetStdHandle(STD_OUTPUT_HANDLE), _Color);
 }
 
-void OnDrawText(const char* _str, const int _x, const int _y, const int _Color)
+void OnDrawText(const char* _str, const float _x, const float _y, const int _Color)
 {
 	SetCursorPosition(_x, _y);
 	SetTextColor(_Color);
 	cout << _str;
 }
 
-void OnDrawText(const int _Value, const int _x, const int _y, const int _Color)
+void OnDrawText(const int _Value, const float _x, const float _y, const int _Color)
 {
 	SetCursorPosition(_x, _y);
 	SetTextColor(_Color);
@@ -123,7 +123,7 @@ void HideCursor(const bool _Visible)
 		GetStdHandle(STD_OUTPUT_HANDLE), &CursorInfo);
 }
 
-void Collision(const Object* _ObjectA, const Object* _ObjectB)
+bool Collision(const Object* _ObjectA, const Object* _ObjectB)
 {
 	// ** (_Object->TransInfo.Position.x + _Object->TransInfo.Scale.x)   : 우측
 	// ** _Object->TransInfo.Position.x   : 좌측
@@ -131,16 +131,28 @@ void Collision(const Object* _ObjectA, const Object* _ObjectB)
 	if ((_ObjectA->TransInfo.Position.x + _ObjectA->TransInfo.Scale.x) > _ObjectB->TransInfo.Position.x &&
 		(_ObjectB->TransInfo.Position.x + _ObjectB->TransInfo.Scale.x) > _ObjectA->TransInfo.Position.x &&
 		_ObjectA->TransInfo.Position.y == _ObjectB->TransInfo.Position.y)
-		OnDrawText((char*)"충돌 입니다.", 55, 0, 14);
+		return true;
+	return false;
 }
 
-Object* CreateBullet(const int _x, const int _y)
+Object* CreateBullet(const float _x, const float _y)
 {
 	// ** Bullet를 생성 및 동적할당.
 	Object* _Object = new Object;
 
 	// ** 초기화 시 _x 와 _y 는 const 값으로 받아오면, 함수 내부에서 다른 값으로 변경 되지 않기때문에 안전하다.
 	Initialize(_Object, (char*)"장풍!", _x + 2, _y);
+
+	return _Object;
+}
+
+Object* CreateEnemy(const float _x, const float _y)
+{
+	// ** Bullet를 생성 및 동적할당.
+	Object* _Object = new Object;
+
+	// ** 초기화 시 _x 와 _y 는 const 값으로 받아오면, 함수 내부에서 다른 값으로 변경 되지 않기때문에 안전하다.
+	Initialize(_Object, (char*)"홋", _x, _y);
 
 	return _Object;
 }
--- c/c_frameworkstudy0.3/c_frameworkstudy0.3/main.cpp
+++ c/c_frameworkstudy0.3/c_frameworkstudy0.3/main.cpp
@@ -28,8 +28,8 @@ int main(void)
 		BackGround[i].Info.Option = 0;
 
 		// ** 좌표를 랜덤으로 설정함.
-		BackGround[i].TransInfo.Position.x = rand() % 100 + 10;
-		BackGround[i].TransInfo.Position.y = rand() % 26 + 1;
+		BackGround[i].TransInfo.Position.x = float(rand() % 100 + 10);
+		BackGround[i].TransInfo.Position.y = float(rand() % 26 + 1);
 
 		// ** 배경으로 사용될 텍스처를 설정.
 		BackGround[i].Info.Texture = (char*)"*";
@@ -46,13 +46,16 @@ int main(void)
 	Initialize(Player, (char*)"옷/", 30, 10);
 
 	// ** Enemy선언 및 동적할당.
-	Object* Enemy = new Object;
+	Object* Enemy[32]; //= new Object;
 
 	// ** Enemy 초기화
-	Initialize(Enemy, (char*)"홋", 80, 10);
+	for (int i = 0; i < 32; ++i)
+		Enemy[i] = nullptr;
+
 
 	// ** 현재 시간으로 초기화.
 	ULONGLONG Time = GetTickCount64();
+	ULONGLONG EnemyTime = GetTickCount64();
 
 	int Score = 0;
 
@@ -60,6 +63,19 @@ int main(void)
 	Object* Bullet[128] = { nullptr };
 
 	int BulletCount = 0;
+
+
+
+	// ** Test
+	Object* Temp = new Object;
+
+	Temp->TransInfo.Position.x = 80;
+	Temp->TransInfo.Position.y = 10;
+
+	Temp->Info.Texture = (char*)"★";
+
+
+
 
 	// ** 출력
 	while (true)
@@ -87,27 +103,62 @@ int main(void)
 				BackGround[i].Info.Color = rand() % 8 + 1;
 			}
 
+			/*
+			// ** Enemy 생성.
+			if (EnemyTime + 1500 < GetTickCount64())
+			{
+				EnemyTime = GetTickCount64();
 
+				for (int i = 0; i < 32; ++i)
+				{
+					if (Enemy[i] == nullptr)
+					{
 
+						// ** 랜덤값을 초기화 해줌. 큰 값이 나올수 있도록 Time값끼리 곱해줄 것이지만,
+						// ** for문이 빠르게 돌게되면 Time의 증가값보다 빠를수 있기때문에,
+						// ** 랜덤값이라고 하더라도 연속으로 같은 값이 나올수 있음.
+						// ** i의 값을 곱하고 더해줌으로써 같은 값이 나오지 않도록 해줌.
+
+						srand((GetTickCount() + i * i) * GetTickCount());
+						Enemy[i] = CreateEnemy(115, rand()%30);
+						break;
+					}
+				}
+			}
+			*/
+
+			// ** 숙제 : Bullet & Enemy 충돌 구현.
 			for (int i = 0; i < 128; ++i)
 			{
 				if (Bullet[i] != nullptr)
 				{
-					if ((Bullet[i]->TransInfo.Position.x + Bullet[i]->TransInfo.Scale.x) >= 120)
+					for (int j = 0; j < 32; ++j)
 					{
-						delete Bullet[i];
-						Bullet[i] = nullptr;
+						if (Enemy[j] != nullptr)
+						{
+							if (Collision(Enemy[j], Bullet[i]))
+							{
+								delete Enemy[j];
+								Enemy[j] = nullptr;
 
-						--BulletCount;
+								delete Bullet[i];
+								Bullet[i] = nullptr;
+								break;
+							}
+						}
 					}
+
+					if (Bullet[i] != nullptr)
+						if ((Bullet[i]->TransInfo.Position.x + Bullet[i]->TransInfo.Scale.x) >= 120)
+						{
+							delete Bullet[i];
+							Bullet[i] = nullptr;
+						}
 				}
 			}
 
-			Collision(Player, Enemy);
-
+			// ** 키 입력
 			UpdateInput(Player);
-
-
 
 			// ** [Space] 키를 입력받음.
 			if (GetAsyncKeyState(VK_SPACE))
@@ -123,34 +174,103 @@ int main(void)
 					}
 				}
 
+
+
+			// ** Player 출력
 			OnDrawText(Player->Info.Texture,
 				Player->TransInfo.Position.x,
 				Player->TransInfo.Position.y,
 				10);
 
-			OnDrawText(Enemy->Info.Texture,
-				Enemy->TransInfo.Position.x,
-				Enemy->TransInfo.Position.y,
+			// ** Temp 출력
+			OnDrawText(Temp->Info.Texture,
+				Temp->TransInfo.Position.x,
+				Temp->TransInfo.Position.y,
 				12);
+
+
+			// ** 333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+			// ** 피타고라스 정의(플레이어 따라가게)
+
+			float x = Player->TransInfo.Position.x - Temp->TransInfo.Position.x;
+			float y = Player->TransInfo.Position.y - Temp->TransInfo.Position.y;
+
+
+			// ** sqrt : 제곱근 함수
+			float Length = sqrt((x * x) + (y * y));
+
+			Vector3 Direction = Vector3(x / Length, y / Length);
+
+			Temp->TransInfo.Position.x += Direction.x;
+			Temp->TransInfo.Position.y += Direction.y;
+
+
+
+
+
+
+
+
+
+
+
+
+			OnDrawText((char*)"Length : ", float(60 - strlen("Score : ")), 2.0f);
+			OnDrawText((int)Length, 60.0f, 2.0f);
+
+
+
+
+
+
+
+
+
+
+
+
+
+			for (int i = 0; i < 32; ++i)
+			{
+				if (Enemy[i])
+				{
+					// ** Enemy 이동
+					Enemy[i]->TransInfo.Position.x--;
+
+					// **Enemy 출력
+					OnDrawText(Enemy[i]->Info.Texture,
+						Enemy[i]->TransInfo.Position.x,
+						Enemy[i]->TransInfo.Position.y,
+						12);
+
+					// ** Enemy 스크린 충돌
+					if (Enemy[i]->TransInfo.Position.x <= 0)
+					{
+						// ** 충돌하면 삭제
+						delete Enemy[i];
+						Enemy[i] = nullptr;
+					}
+				}
+			}
 
 			// ** Bullet 출력
 			for (int i = 0; i < 128; ++i)
 			{
 				if (Bullet[i])
 				{
+					// ** Bullet 이동
 					Bullet[i]->TransInfo.Position.x += 2;
 
+					// ** Bullet 출력
 					OnDrawText(Bullet[i]->Info.Texture,
 						Bullet[i]->TransInfo.Position.x,
 						Bullet[i]->TransInfo.Position.y);
 				}
 			}
 
-			OnDrawText((char*)"Bullet Count : ", 95, 1);
-			OnDrawText(BulletCount, 95 + strlen("Bullet Count : "), 1);
-
-			OnDrawText((char*)"Score : ", 60 - strlen("Score : "), 1);
-			OnDrawText(++Score, 60, 1);
+			// ** Score 출력
+			OnDrawText((char*)"Score : ", float(60 - strlen("Score : ")), 1.0f);
+			OnDrawText(++Score, 60.0f, 1.0f);
 		}
 	}
 
