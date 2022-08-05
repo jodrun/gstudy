c353e2a548a14fd236a644df8b2a2588dc3691e9
jodrun <jodrun@naver.com>
2022-08-05 15:54:17 +0900

    c++framework_master0.6

--- c/c++framework_master1/Framework/Map.h
+++ c/c++framework_master1/Framework/Map.h
@@ -4,14 +4,14 @@
 class Map : public Object
 {
 public:
-	char StrMaze[101][30] = {};
+	char StrMaze[31][101] = {};
 public:
 	virtual void Start()override;
 	virtual int  Update()override;
 	virtual void Render()override;
 	virtual void Release()override;
 public:
-	void SetMaze(char _Maze[101][30]);
+	void SetMaze(char _Maze[31][101]);
 public:
 	Map();
 	virtual ~Map();
